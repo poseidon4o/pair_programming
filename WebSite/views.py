de493d48f40062ff35fa87ebc3ce3acddb52d0e4
SashkU <endisnear@abv.bg>
2013-10-26 19:58:54 +0300

    asd

--- WebSite/views.py
+++ WebSite/views.py
@@ -1,16 +1,16 @@
-# Create your views here.
-
+from django.template import Template
 
 def list_users(request):
     return ""
 
 def login_page(request):
 
-    if request.method == "POST" and request.POST.has_key("username") and \
-    request.POST.has_key("password"):
-
-    elif request.method == "GET":
-        
+    if request.method == "POST":
+        if "username" in request.POST and "password" in request.POST:
+            inta = 5
+    else:
+        tmp = Template('templates/index.html')
+        tmp.render()
 
     return ""
 
