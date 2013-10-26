8fefe81b3e0f7682a97e679a757e4d9e2b3d2f53
Lyubomir <poseidon4o.random@gmail.com>
2013-10-27 00:43:11 +0300

    Added events and model fix

---
+++ web_site/events.py
@@ -0,0 +1,25 @@
+
+from django.shortcuts import get_object_or_404
+from django_socketio import events
+
+from web_site.models import Pair
+
+
+@events.on_connect
+def connect(request, socket, context):
+    print "connect"
+
+
+@events.on_message(channel="^pair-code-")
+def message(request, socket, context, message):
+    """
+    Event handler for pair data channel
+    """
+    if 'pair_id' not in message or 'user_id' not in message:
+        return
+
+    pair = get_object_or_404(Pair, id=message['pair_id'])
+
+    if pair.l_u_id != int(message['user_id']) and pair.r_u_id != int(message['user_id']):
+        return
+
--- web_site/models.py
+++ web_site/models.py
@@ -1,3 +1,12 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Pair(models.Model):
+    pairname = models.CharField(max_length=50)
+    task = models.CharField(max_length=50)
+    prog_language = models.CharField(max_length=25)
+    l_u_id = models.ForeignKey(User, related_name='left_user_id')
+    r_u_id = models.ForeignKey(User, related_name='right_user_id')
+    turn = models.CharField(max_length=10) #left or right
