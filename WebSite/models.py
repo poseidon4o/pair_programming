242347cbb339c78b302132bc34c6e18080e320e0
lorddessan <lorddessan91@gmail.com>
2013-10-26 18:37:07 +0300

    Added User and Pair

--- WebSite/models.py
+++ WebSite/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class User(User.objects):
+    def join_pair(self, Pair):
+        Pair.l_u_id = self.id
+        return Pair
+
+class Pair(models.Model):
+    pairname = models.CharField(max_length=50)
+    task = models.CharField(max_length=50)
+    prog_language = enumerate('C', 'C++', 'C#', 'Python', 'PHP', 'HTML', 'Java')
+    l_u_id = models.ForeignKey(User)
+    r_u_id = models.ForeignKey(User)
+
+    def set_details(self, pairname, task, prog_language):
+        self.task = task
+        self.pairname = pairname
+        self.prog_language = prog_language
+
+    def join_user(self, User):
+        self.r_u_id = User.id
