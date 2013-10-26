27fb7870593db8ae25f324730d2d27b24a22bff1
Lyubomir <poseidon4o.random@gmail.com>
2013-10-26 19:33:46 +0300

    Added register action and removed user model

--- WebSite/models.py
+++ WebSite/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class User(User.objects):
-    def join_pair(self, Pair):
-        Pair.l_u_id = self.id
-        return Pair
+
+#class UUser(User.objects):
+#    def join_pair(self, Pair):
+#        Pair.l_u_id = self.id
+#        return Pair
 
 class Pair(models.Model):
     pairname = models.CharField(max_length=50)
     task = models.CharField(max_length=50)
-    prog_language = enumerate('C', 'C++', 'C#', 'Python', 'PHP', 'HTML', 'Java')
-    l_u_id = models.ForeignKey(User)
-    r_u_id = models.ForeignKey(User)
+    prog_language = enumerate(['C', 'C++', 'C#', 'Python', 'PHP', 'HTML', 'Java'])
+    l_u_id = models.ForeignKey(User, related_name='left_user_id')
+    r_u_id = models.ForeignKey(User, related_name='right_user_id')
 
     def set_details(self, pairname, task, prog_language):
         self.task = task
--- WebSite/views.py
+++ WebSite/views.py
@@ -1,16 +1,26 @@
 # Create your views here.
+#from WebSite.models import UUser
+from django.contrib.auth.models import User
+from django.template.response import TemplateResponse
 
 
 def list_users(request):
     return ""
 
+
 def login_page(request):
-
-    if request.method == "POST":
-
-    elif request.method == "GET":
     return ""
+
 
 def register_page(request):
-    return ""
+    if request.method == 'POST':
+        if 'username' in request.POST and 'password' in request.POST and 'password_again' in request.POST:
+            #u_data = User.validate(request.POST)
+            u_data = {'username': 1, 'password': 2}
+            if u_data is not None:
+                user = User.create_user(u_data['username'], u_data['password'])
+                # TODO: add user to session
+                return TemplateResponse(request, 'lobby.html')
+    return TemplateResponse(request, 'register.html')
+
 
