bf0f21e60df4d3d4f9a8fe5f3c8535f9e048ac9e
Lyubomir <poseidon4o.random@gmail.com>
2013-10-28 02:19:06 +0200

    Static dir

--- pp/settings.py
+++ pp/settings.py
@@ -65,7 +65,6 @@ STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
     '/home/poseidon4o/python/pp/static/',
-    #'/home/endisnear/pp/static/',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
